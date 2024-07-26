ba1307408acb39b0021ca65bfbfcbadf32435f16
Felipe Costa <felipestscosta@gmail.com>
2024-07-26 15:06:58 -0300

    feature: finalizando a parte de estoques ao subir pro bling

--- src/pages/api/bling-estoques.ts
+++ src/pages/api/bling-estoques.ts
@@ -3,52 +3,31 @@ import axios from 'axios';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { token } = req.query;
-  const { data } = req.body;
   const tipoMetodo = req.method;
+  const data = req.body;
 
-  // Adiciona Novo Estoque
   if(tipoMetodo === 'POST'){
-    const dataNovoProduto = req.body;
+    try {
+      const retorno = await axios.post(
+        'https://bling.com.br/Api/v3/estoques',
+        {
+          "produto": {
+            "id": data.id
+          },
+          "deposito": {
+            "id": 14887659608
+          },
+          "operacao": "B",
+          "quantidade": 1000,
+        }
+        ,
+        { headers: {Authorization: `Bearer ${token}`} }
+      )
 
-    console.log(dataNovoProduto)
-
-    // try {
-
-    //   // Adiciona estoque
-    //   const estoque = data.quantidade;
-    //   const idProduto = data.id;
-
-    //   const options = {
-    //     method: 'POST',
-    //     url: 'https://bling.com.br/Api/v3/estoques',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       Authorization: `Bearer ${token}`
-    //     },
-    //     data: {
-    //       produto: {id: idProduto},
-    //       deposito: {id: 14887659608}, // Deposito Padrão
-    //       operacao: 'B', // B = Balanço
-    //       preco: 0,
-    //       custo: 0,
-    //       quantidade: parseFloat(estoque),
-    //       observacoes: 'Estoque adicionado automaticamente pelo Auto Register'
-    //     }
-    //   };
-
-    //   try {
-    //     await axios.request(options).then(function(res) {
-    //       console.log(res)
-    //     });        
-    //   } catch (error) {
-    //     console.log("ERROOOOOO ===============> ", error)
-    //   }
-
-    //   res.status(201).json({ produto_cadastrado: idProduto });
-    // } catch (error: any) {
-    //   console.log(error.response)
-    //   res.status(500).json({ erro: error });
-    // }
+      res.status(201).json('Estoque Adicionado');
+    } catch (error: any) {
+      res.status(500);
+    }
   }
 
   
