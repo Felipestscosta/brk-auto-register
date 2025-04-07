8655fcdb8057037a61b1d032031495b56e0c1e94
Felipe Costa <felipestscosta@gmail.com>
2025-04-07 09:00:46 -0300

    feature: Corrigir posicionamento do prefixo 'ALL_'

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -945,14 +945,21 @@ export default function Home() {
       var produtosDesmembrados = [];
       var produtoPaiAll = todosOsProdutos[0];
 
-      todosOsProdutos = todosOsProdutos.filter((produto) => produto.codigo_pai !== "" && !produto.codigo.includes("I") || !produto.codigo.includes("BL"));
-      todosOsProdutos[0].codigo = `${todosOsProdutos[0].codigo}_ALL`;
+      todosOsProdutos = todosOsProdutos.filter((produto) => produto.codigo_pai !== "" 
+      && !produto.codigo.includes("I") 
+      || !produto.codigo.includes("BL"));
+
+      todosOsProdutos[0].codigo = `ALL_${todosOsProdutos[0].codigo}`;
       todosOsProdutos[0].descricao = data.titulo_all;
 
       todosOsProdutos.map((produto, index) => {
-        return (index !== 0) && produto.codigo_pai !== ""
-          ? produto.codigo_pai = todosOsProdutos[0].codigo
-          : null;
+        if(index !== 0 && produto.codigo_pai !== "") {
+          produto.codigo_pai = todosOsProdutos[0].codigo;
+          produto.codigo = `ALL_${produto.codigo}`
+        }
+        // return (index !== 0) && produto.codigo_pai !== ""
+        //   ? produto.codigo_pai = todosOsProdutos[0].codigo
+        //   : null;
       });
       
       produtosDesmembrados.push(...todosOsProdutos.filter((produto) => produto.codigo_pai !== ""));  
@@ -975,7 +982,7 @@ export default function Home() {
     try {
       if (tipoCadastro === "planilha" || tipoCadastro === "codigo_all") {
         //console.log("Dados da Planilha:", todosOsProdutos);
-        geraPlanilha(todosOsProdutos, "cadastro-bling");
+        //geraPlanilha(todosOsProdutos, "cadastro-bling");
       } else if (tipoCadastro === "bling") {
         //console.log("Dados do Bling:", dadosBling);
         // saveProdutos(dadosBling);
